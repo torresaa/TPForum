f2e0e87692ea0b738c88cd9dab88887c78d0d3d4
Aquiles Ricardo Torres Alvarez <aquiles-ricardo.torres-alvarez@ensimag.grenoble-inp.fr>
2014-11-10 11:23:20 +0100

    Init Omar

--- src/ForumImpl.java
+++ src/ForumImpl.java
@@ -33,7 +33,7 @@ final public class ForumImpl extends UnicastRemoteObject implements Forum {
  */
   public synchronized HashMap enter (Intervenant intervenant, String prenom, String nom)throws RemoteException{	
         System.out.println('[Forum Impl] enter ');
-        //didon on est là
+        //did888
         // OUI on est là
   	// TO DO
   	return null; // CETTE LIGNE EST A CHANGER
